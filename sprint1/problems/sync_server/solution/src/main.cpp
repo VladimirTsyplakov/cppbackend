99e55264a152fe1467aa2c79ee60087e9a1d149a
VladimirTsyplakov <117277589+VladimirTsyplakov@users.noreply.github.com>
2024-05-01 23:34:58 +0300

    Add files via upload

--- sprint1/problems/sync_server/solution/src/main.cpp
+++ sprint1/problems/sync_server/solution/src/main.cpp
@@ -52,17 +52,13 @@ StringResponse HandleRequest(StringRequest&& req) {
 	if(ques == "GET"){
 		std::string_view tar = req.target();
     // Здесь можно обработать запрос и сформировать ответ, но пока всегда отвечаем: Hello
-//		greet = "<strong>Hello</strong>";
-		greet = "Hello";
+		greet = "<strong>Hello</strong>";
 		tar.remove_prefix(1);
-//		greet.insert(13, ", ");
-		greet.insert(5, ", ");
-//		greet.insert(15, tar);
-		greet.insert(7, tar);
+		greet.insert(13, ", ");
+		greet.insert(15, tar);
 			 }
 	if(ques != "GET" && ques != "HEAD"){
-//	greet = "<strong>Invalid method</strong"; 
-	greet = "Invalid method";
+	greet = "<strong>Invalid method</strong"; 
 	stat = http::status::method_not_allowed;}
 
 	return text_response(stat, greet);
